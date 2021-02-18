6f21f4350adbfc9cba63c3acb508226a03b33f34
imxood <imxood@gmail.com>
2021-02-18 18:43:27 +0800

    .

--- apps/f767ig_app/src/usb.c
+++ apps/f767ig_app/src/usb.c
@@ -1,9 +1,7 @@
 
-#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#define LOG_LEVEL CONFIG_LOG_MAX_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(test_hid);
-
-#include <zephyr.h>
 
 #include <usb/usb_device.h>
 #include <usb/class/usb_hid.h>
@@ -11,11 +9,24 @@ LOG_MODULE_REGISTER(test_hid);
 #define REPORT_ID_1 0x01
 #define REPORT_ID_2 0x02
 
-static const struct device *hid_dev;
-static K_SEM_DEFINE(usb_configured, 0, 1);
+int usb_waitForConfigured(k_timeout_t timeout);
+int usb_waitForSuspended(k_timeout_t timeout);
+int usb_waitForReadReady(k_timeout_t timeout);
+
+static const struct device *hid0_dev;
+static K_SEM_DEFINE(hid0_out_ready, 0, 1);
+
+static struct usb_status
+{
+    enum usb_dc_status_code cur_status;
+    enum usb_dc_status_code pre_status;
+    enum usb_dc_status_code status;
+    struct k_sem usb_configured;
+    struct k_sem usb_suppended;
+} usb_status;
 
 /* Some HID sample Report Descriptor */
-static const uint8_t hid_report_desc[] = {
+static const uint8_t hid0_report_desc[] = {
     /* 0x05, 0x01,		USAGE_PAGE (Generic Desktop)		*/
     HID_GI_USAGE_PAGE,
     USAGE_GEN_DESKTOP,
@@ -66,90 +77,152 @@ static const uint8_t hid_report_desc[] = {
     HID_MI_COLLECTION_END,
 };
 
-int debug_cb(const struct device *dev, struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
+static inline void usb_init_status()
+{
+    usb_status.cur_status = USB_DC_UNKNOWN;
+    usb_status.pre_status = USB_DC_UNKNOWN;
+    k_sem_init(&usb_status.usb_configured, 0, 1);
+    k_sem_init(&usb_status.usb_suppended, 0, 1);
+}
+
+static inline void usb_set_status(enum usb_dc_status_code cb_status)
+{
+    if (cb_status == USB_DC_CONFIGURED)
+    {
+        usb_status.pre_status = usb_status.cur_status;
+        usb_status.cur_status = USB_DC_CONFIGURED;
+        k_sem_give(&usb_status.usb_configured);
+    }
+    else if (cb_status == USB_DC_SUSPEND)
+    {
+        if (usb_status.cur_status == USB_DC_CONFIGURED)
+        {
+            k_sem_give(&usb_status.usb_suppended);
+        }
+		usb_status.pre_status = usb_status.cur_status;
+        usb_status.cur_status = USB_DC_SUSPEND;
+    }
+}
+
+static void usb_dc_status_cb(enum usb_dc_status_code cb_status, const uint8_t *param)
+{
+    usb_set_status(cb_status);
+}
+
+int usb_waitForConfigured(k_timeout_t timeout) {
+	return k_sem_take(&usb_status.usb_configured, timeout);
+}
+
+int usb_waitForSuspended(k_timeout_t timeout) {
+	return k_sem_take(&usb_status.usb_suppended, timeout);
+}
+
+int usb_waitForReadReady(k_timeout_t timeout) {
+	return k_sem_take(&hid0_out_ready, timeout);
+}
+
+static int debug_cb(const struct device *dev, struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
 {
     LOG_DBG("Debug callback");
     return -ENOTSUP;
 }
 
-int set_idle_cb(const struct device *dev, struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
+static int set_idle_cb(const struct device *dev, struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
 {
     LOG_DBG("Set Idle callback");
     return 0;
 }
 
-int get_report_cb(const struct device *dev, struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
+static int get_report_cb(const struct device *dev, struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
 {
     LOG_DBG("Get report callback");
     return 0;
 }
 
-static struct hid_ops ops = {
-    .get_report = get_report_cb,
-    .get_idle = debug_cb,
-    .get_protocol = debug_cb,
-    .set_report = debug_cb,
-    .set_idle = set_idle_cb,
-    .set_protocol = debug_cb,
-};
-
-static void hid_thread(void *p1, void *p2, void *p3)
+static void hid0_out_ready_cb(const struct device *dev)
 {
-    uint8_t report_1[2] = {REPORT_ID_1, 0x00};
-    int ret, wrote;
-
-	k_sem_take(&usb_configured, K_FOREVER);
-
-    LOG_DBG("Starting hid loop");
-
-    while (true)
-    {
-        k_sleep(K_SECONDS(1));
-
-        report_1[1]++;
-
-        ret = hid_int_ep_write(hid_dev, report_1, sizeof(report_1), &wrote);
-        LOG_DBG("Wrote %d bytes with ret %d", wrote, ret);
-    }
+    LOG_INF("usb hid0 out ready!");
+    k_sem_give(&hid0_out_ready);
 }
 
-static void fn_usb_dc_status_callback(enum usb_dc_status_code cb_status, const uint8_t *param)
-{
-    LOG_INF("cb_status: %d\n", cb_status);
-	if (cb_status == USB_DC_CONFIGURED) {
-		LOG_INF("usb dc configured");
-		k_sem_give(&usb_configured);
-	}
-}
+static struct hid_ops ops = {.get_report = get_report_cb,
+                             .get_idle = debug_cb,
+                             .get_protocol = debug_cb,
+                             .set_report = debug_cb,
+                             .set_idle = set_idle_cb,
+                             .set_protocol = debug_cb,
+                             .int_out_ready = hid0_out_ready_cb};
 
 static int sys_init_usb(const struct device *dev)
 {
-    int ret = usb_enable(fn_usb_dc_status_callback);
+    usb_init_status();
+
+    int ret = usb_enable(usb_dc_status_cb);
     if (ret)
     {
         LOG_ERR("usb enable failed\n");
-		return -1;
+        return -1;
     }
-	LOG_INF("usb enable success\n");
+    LOG_INF("usb enable success\n");
     return 0;
 }
 
 static int sys_init_usb_hid(const struct device *dev)
 {
-    hid_dev = device_get_binding("HID_0");
-    if (hid_dev == NULL)
+    hid0_dev = device_get_binding("HID_0");
+    if (hid0_dev == NULL)
     {
         LOG_ERR("Cannot get HID_0 Device");
         return -1;
     }
 
-    usb_hid_register_device(hid_dev, hid_report_desc, sizeof(hid_report_desc), &ops);
-    usb_hid_init(hid_dev);
-	LOG_INF("usb hid registered");
+    usb_hid_register_device(hid0_dev, hid0_report_desc, sizeof(hid0_report_desc), &ops);
+    usb_hid_init(hid0_dev);
+    LOG_INF("usb hid registered");
     return 0;
 }
 
-K_THREAD_DEFINE(hid_thread_id, 1024, hid_thread, NULL, NULL, NULL, 1, 0, 0);
+static void hid0_read_thread(void *p1, void *p2, void *p3)
+{
+    uint8_t report[64];
+    int ret, read;
+
+    if (hid0_dev == NULL)
+    {
+        LOG_ERR("HID_0 Device is NULL, read hid read thread failed");
+        return;
+    }
+
+    while (true)
+    {
+        LOG_INF("wait for hid0 out ready ..");
+        memset(report, 0, sizeof(report));
+		usb_waitForReadReady(K_FOREVER);
+
+        printk("hid0 out msg receoved!\n");
+        ret = hid_int_ep_read(hid0_dev, report, sizeof(report), &read);
+        LOG_DBG("Wrote %d bytes with ret %d", read, ret);
+        LOG_HEXDUMP_INF(report, sizeof(report), "read hid data");
+    }
+}
+
+static K_THREAD_STACK_DEFINE(usb_stack, 1024);
+
+static void usb_thread(void *p1, void *p2, void *p3)
+{
+    static struct k_thread hid0_read_tid;
+    while (1)
+    {
+        /* 等待 usb setup */
+		usb_waitForConfigured(K_FOREVER);
+        k_thread_create(&hid0_read_tid, usb_stack, 1024, hid0_read_thread, NULL, NULL, NULL, 1, 0, K_NO_WAIT);
+        /* 拔出 usb设备 或 usb设备 suppend时 删除线程, 等待 usb 重新 setup */
+		usb_waitForSuspended(K_FOREVER);
+        k_thread_abort(&hid0_read_tid);
+    }
+}
 
 SYS_INIT(sys_init_usb_hid, POST_KERNEL, 0);
-SYS_INIT(sys_init_usb, POST_KERNEL, 1);
+SYS_INIT(sys_init_usb, POST_KERNEL, CONFIG_APPLICATION_INIT_PRIORITY);
+
+K_THREAD_DEFINE(hid_thread_id, 1024, usb_thread, NULL, NULL, NULL, 1, 0, 0);
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-## 编译, 烧写
+# zephyr RTOS 开发
 
 ### 初始化
 
@@ -8,6 +8,8 @@
 
 ### 编译 stm32f103c8
 
+Blue pill 开发板
+
     cd zephyr_develop/apps/f103c8_app
 
     west build -b stm32f103c8 -- -DBOARD_ROOT=../.. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
@@ -16,6 +18,8 @@
     west flash
 
 ### 编译 stm32f767ig
+
+正点原子的开发板
 
     cd zephyr_develop/apps/f767ig_app
 
